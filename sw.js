4c30de7f00d7c7b0e552421518213d4318f1cc7c
Galsoilson1972 <galsoilson@gmail.com>
2025-05-13 16:08:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'flashcards-v1';
+const filesToCache = [
+  '/flashcards-pwa/',
+  '/flashcards-pwa/index.html',
+  '/flashcards-pwa/manifest.json',
+  '/flashcards-pwa/icon-192.png',
+  '/flashcards-pwa/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
